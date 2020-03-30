09445f874b33d74700a9312e51beef61aefe87dd
Sascha Majewsky <sascha.majewsky@pm.me>
2020-03-30 13:01:01 +0200

    Added contentScript.js for html DOM removal and icons for logo preview, also updated the readme

---
+++ js/contentScript.js
@@ -0,0 +1,8 @@
+let clarificationBoxTitle = document.querySelector(".content-title").innerText;
+let clarificationBox = document.getElementById("clarify-box");
+
+// YouTube doesn't reload the page when you click a new video so the current display: none is persistent longer than you might want to. This could be extended with a background script that listens via chrome.tabs.onUpdated.addListener for URL changes and sends a message to this contentScript via chrome.extension.onMessage.addListener just whenever a new YouTube video was loaded.
+
+clarificationBox && clarificationBoxTitle === "Coronavirus"
+  ? (clarificationBox.style.display = "none")
+  : console.log("No coronavirus on this page detected!");
